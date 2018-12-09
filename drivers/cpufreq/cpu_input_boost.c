9e16fef76e2c629970aca81f0b1053e80f1fa34a
kdrag0n <dragon@khronodragon.com>
2018-12-09 11:35:50 -0800

    drm/drm_atomic: Boost only if last interaction was within 3s
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -184,7 +184,6 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
-<<<<<<< HEAD
 	
 	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	do_stune_boost("top-app", dynamic_stune_boost);
@@ -192,9 +191,6 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 	
 	if (dynamic_sched_boost)
 		sched_set_boost(2);
-=======
-	sched_set_boost(2);
->>>>>>> 4ce0cce15769 (cpu_input_boost: Enable sched_boost on input events)
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
 			      msecs_to_jiffies(input_boost_duration)))
 		wake_up(&b->boost_waitq);
@@ -251,12 +247,8 @@ static void input_unboost_worker(struct work_struct *work)
 					   typeof(*b), input_unboost);
 
 	clear_bit(INPUT_BOOST, &b->state);
-<<<<<<< HEAD
 	if (dynamic_sched_boost)
 		sched_set_boost(0);
-=======
-	sched_set_boost(0);
->>>>>>> 4ce0cce15769 (cpu_input_boost: Enable sched_boost on input events)
 	wake_up(&b->boost_waitq);
 	
 	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
@@ -408,7 +400,6 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
-<<<<<<< HEAD
 	do_lp_cpuset();
 	
 	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
@@ -417,9 +408,6 @@ static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 	
 	if (dynamic_sched_boost)
 		sched_set_boost(0);
-=======
-	sched_set_boost(0);
->>>>>>> 4ce0cce15769 (cpu_input_boost: Enable sched_boost on input events)
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
