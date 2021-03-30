53241726c858699e1aef3193116deefd2c0991ba
Greg Kroah-Hartman <gregkh@google.com>
2021-03-30 17:05:07 +0200

    Merge 4.14.228 into android-4.14-stable
    Changes in 4.14.228
    	net: fec: ptp: avoid register access when ipg clock is disabled
    	powerpc/4xx: Fix build errors from mfdcr()
    	atm: eni: dont release is never initialized
    	atm: lanai: dont run lanai_dev_close if not open
    	Revert "r8152: adjust the settings about MAC clock speed down for RTL8153"
    	ixgbe: Fix memleak in ixgbe_configure_clsu32
    	net: tehuti: fix error return code in bdx_probe()
    	sun/niu: fix wrong RXMAC_BC_FRM_CNT_COUNT count
    	gpiolib: acpi: Add missing IRQF_ONESHOT
    	nfs: fix PNFS_FLEXFILE_LAYOUT Kconfig default
    	NFS: Correct size calculation for create reply length
    	net: hisilicon: hns: fix error return code of hns_nic_clear_all_rx_fetch()
    	net: wan: fix error return code of uhdlc_init()
    	atm: uPD98402: fix incorrect allocation
    	atm: idt77252: fix null-ptr-dereference
    	sparc64: Fix opcode filtering in handling of no fault loads
    	u64_stats,lockdep: Fix u64_stats_init() vs lockdep
    	drm/radeon: fix AGP dependency
    	nfs: we don't support removing system.nfs4_acl
    	ia64: fix ia64_syscall_get_set_arguments() for break-based syscalls
    	ia64: fix ptrace(PTRACE_SYSCALL_INFO_EXIT) sign
    	squashfs: fix inode lookup sanity checks
    	squashfs: fix xattr id and id lookup sanity checks
    	arm64: dts: ls1046a: mark crypto engine dma coherent
    	arm64: dts: ls1012a: mark crypto engine dma coherent
    	arm64: dts: ls1043a: mark crypto engine dma coherent
    	ARM: dts: at91-sama5d27_som1: fix phy address to 7
    	dm ioctl: fix out of bounds array access when no devices
    	bus: omap_l3_noc: mark l3 irqs as IRQF_NO_THREAD
    	libbpf: Fix INSTALL flag order
    	macvlan: macvlan_count_rx() needs to be aware of preemption
    	net: dsa: bcm_sf2: Qualify phydev->dev_flags based on port
    	e1000e: add rtnl_lock() to e1000_reset_task
    	e1000e: Fix error handling in e1000_set_d0_lplu_state_82571
    	net/qlcnic: Fix a use after free in qlcnic_83xx_get_minidump_template
    	ftgmac100: Restart MAC HW once
    	can: peak_usb: add forgotten supported devices
    	can: c_can_pci: c_can_pci_remove(): fix use-after-free
    	can: c_can: move runtime PM enable/disable to c_can_platform
    	can: m_can: m_can_do_rx_poll(): fix extraneous msg loss warning
    	mac80211: fix rate mask reset
    	net: cdc-phonet: fix data-interface release on probe failure
    	net: stmmac: dwmac-sun8i: Provide TX and RX fifo sizes
    	drm/msm: fix shutdown hook in case GPU components failed to bind
    	arm64: kdump: update ppos when reading elfcorehdr
    	net/mlx5e: Fix error path for ethtool set-priv-flag
    	RDMA/cxgb4: Fix adapter LE hash errors while destroying ipv6 listening server
    	ACPI: scan: Rearrange memory allocation in acpi_device_add()
    	ACPI: scan: Use unique number for instance_no
    	perf auxtrace: Fix auxtrace queue conflict
    	scsi: qedi: Fix error return code of qedi_alloc_global_queues()
    	scsi: mpt3sas: Fix error return code of mpt3sas_base_attach()
    	locking/mutex: Fix non debug version of mutex_lock_io_nested()
    	can: dev: Move device back to init netns on owning netns delete
    	net: sched: validate stab values
    	net: qrtr: fix a kernel-infoleak in qrtr_recvmsg()
    	mac80211: fix double free in ibss_leave
    	ext4: add reclaim checks to xattr code
    	can: peak_usb: Revert "can: peak_usb: add forgotten supported devices"
    	xen-blkback: don't leak persistent grants from xen_blkbk_map()
    	Linux 4.14.228
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I269d7975a72f3d7d0b548aeb64415868d70120f6

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -2129,9 +2129,12 @@ static int msm_pdev_remove(struct platform_device *pdev)
 static void msm_pdev_shutdown(struct platform_device *pdev)
 {
 	struct drm_device *drm = platform_get_drvdata(pdev);
-	struct msm_drm_private *priv = NULL;
         struct drm_device *ddev = platform_get_drvdata(pdev);
-	
+	struct msm_drm_private *priv = drm ? drm->dev_private : NULL;
+
+	if (!priv || !priv->kms)
+		return;
+		
 	drm_atomic_helper_shutdown(drm);
 	if (!ddev) {
 		DRM_ERROR("invalid drm device node\n");
